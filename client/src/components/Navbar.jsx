7bcf85c899b3ae25f3f46e25a8b26023f8e0a0da
mmeitneriumm <mmeitneriumm@gmail.com>
2022-05-20 13:27:14 +0300

    Rebranding

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -22,11 +22,11 @@ const Navbar = () => {
                 <img src={logo} alt="logo" className='w-32' cursor-pointer />
             </div>
             <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
-                {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+                {["Магазин", "Обмен", "Обучение", "Кошельки"].map((item, index) => (
                     <NavbarItem key={item + index} title={item} />
                 ))}
                 <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
-                    Login
+                    Войти
                 </li>
             </ul>
             <div className="flex relative">
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,7 +1,9 @@
+import React, {useContext} from 'react'
 import { AiFillAlipayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
+import { TransactionContext } from "../context/TransactionContext";
 import { Loader } from "./";
 
 const commonStyles =
@@ -18,33 +20,37 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-  const connectWallet = () => {};
-  const handleSubmit = () => {};
+  const {connectWallet, currentAccount, formData, handleChange, sendTransaction} = useContext(TransactionContext)
+  const handleSubmit = () => {
+    const {addressTo, amount, keyword, message} = formData
+    e.preventDefault()
+    if(!addressTo || !amount || !keyword || !message) return;
+    sendTransaction()
+  };
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
         <div className="flex flex-1 justify-start  flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
-            Send Crypto <br /> across the world
+            Платите криптой <br /> за учебу в политехе.
           </h1>
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-            Explore the crypto world. Buy and sell cryptocurrencies easily on
-            Crypto.
+          Хватит отдавать бешенные комиссии жадным банкам, отдавайте лучше нам и майнерам.
           </p>
-          <button
+          {!currentAccount && (<button
             type="button"
             onClick={connectWallet}
             className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546be]"
           >
-            <p className="text-white text-base font-semibold">Connect Wallet</p>
-          </button>
+            <p className="text-white text-base font-semibold">Подключить кошелек</p>
+          </button>)}
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
-            <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
-            <div className={commonStyles}>Security</div>
+            <div className={`rounded-tl-2xl ${commonStyles}`}>Надежность</div>
+            <div className={commonStyles}>Безопасность</div>
             <div className={`rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
             <div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
-            <div className={commonStyles}>Low fees</div>
-            <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
+            <div className={commonStyles}>Низкая плата (нет)</div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>Блокчейн</div>
           </div>
         </div>
         <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
@@ -69,28 +75,28 @@ const Welcome = () => {
               placeholder="Address To"
               name="AddressTo"
               type="text"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
             <Input
               placeholder="Amount (ETH)"
               name="amount"
               type="number"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
             <Input
               placeholder="Keyword (GIF)"
               name="keyword"
               type="text"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
             <Input
               placeholder="EnterMessage"
               name="message"
               type="text"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
             <div className="h-[1px] w-full bg-gray-400 my-2" />
-            {true ? (
+            {false ? (
               <Loader />
             ) : (
               <button
@@ -98,7 +104,7 @@ const Welcome = () => {
                 onClick={handleSubmit}
                 className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
               >
-                Send now
+                Отправить
               </button>
             )}
           </div>
